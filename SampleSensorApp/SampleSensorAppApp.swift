972ecf9e2101aaee9c4744b62b6eac019b33cb33
Tatsuya Mizuguchi <mizuguchi_yho@yahoo.co.jp>
2022-01-10 12:13:01 +0900

    Initial Commit

---
+++ SampleSensorApp/SampleSensorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleSensorAppApp.swift
+//  SampleSensorApp
+//
+//  Created by Tatsuya Mizuguchi on 2022/01/10.
+//
+
+import SwiftUI
+
+@main
+struct SampleSensorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
